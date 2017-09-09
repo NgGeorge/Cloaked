afb554a23a27057ff58adc30b5631e19c1f872a5
George Ng <gngcp@uw.edu>
2017-09-08 23:06:17 -0700

    Changes

--- cloaked.js
+++ cloaked.js
@@ -1,5 +1,15 @@
 window.onload = function () {
 
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      
+        chrome.tabs.executeScript(tabs[0].id, {code:"console.log('test');"});
+    });
+
+
+
   var elements = $('h2');
 
   for (var i = 0; i < elements.length; i++) {
