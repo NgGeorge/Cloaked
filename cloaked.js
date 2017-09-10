37d7458644eb9b047523185ca7dfe7f6ec86410a
George Ng <gngcp@uw.edu>
2017-09-09 21:32:11 -0700

    Pushing progress

--- cloaked.js
+++ cloaked.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function() {
   chrome.tabs.executeScript(null,
-      {code:"var elements = document.querySelectorAll('h2');for (var i = 0; i < elements.length; i++) {elements[i].innerHTML = 'TEST';console.log('test');}"});
+      {code:"var elements = document.querySelectorAll('h2');for (var i = 0; i < elements.length; i++) {elements[i].innerHTML = 'TEST';}"});
 
 });
 
