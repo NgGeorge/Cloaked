f642b65b2af5138f875e416cd5979a78e7fb598a
George Ng <gngcp@uw.edu>
2017-09-09 21:13:23 -0700

    pop up now replaces everything in the page

--- cloaked.js
+++ cloaked.js
@@ -1,20 +1,7 @@
-window.onload = function () {
+document.addEventListener('DOMContentLoaded', function () {
+  chrome.tabs.executeScript(null,
+      {code:"var elements = document.querySelectorAll('h2');for (var i = 0; i < elements.length; i++) {elements[i].innerHTML = 'TEST';console.log('test');}"});
 
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs) {
-      
-        chrome.tabs.executeScript(tabs[0].id, {code:"console.log('test');"});
-    });
-
-
-
-  var elements = $('h2');
-
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].html('TEST');
-  }
-}
+});
 
 
