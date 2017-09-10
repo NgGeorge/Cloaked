73da173e8e37e35097b4e3a4293fc3d0274340e4
George Ng <gngcp@uw.edu>
2017-09-09 22:50:02 -0700

    Saving changes and pseudo code

--- cloaked.js
+++ cloaked.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
-  var elements = $('h2');
-  elements.html('ATEST');
-});
+  // TODO : Traverse the DOM BFS, once you reach the innermost child then start working backwards
+  // call the spoiler replace function on each child.
 
+  // TODO : Put this in a function that takes an element as an argument
+  var elements = $('*'); // Change this to just grab the given element passed in as an argument
+  elements.each(
+    var className = $(this).attr("class").toLower();
+    var id = $(this).attr("id").toLower();
+    if (className == 'destiny' || id == 'destiny' || $(this).text().toLower().includes('destiny')) {
+      $(this).replaceWith(//original element minus any inner content);
+      $(this) // style stuff we can put in
+    }
+  );
+});
 
