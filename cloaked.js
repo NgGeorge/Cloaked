7835b94bf00d707fc68acec83d9d86f8364236e4
George Ng <gngcp@uw.edu>
2017-09-09 21:54:34 -0700

    It's loading properly now hot damn

--- cloaked.js
+++ cloaked.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
-  chrome.tabs.executeScript(null,
-      {code:"var elements = document.querySelectorAll('h2');for (var i = 0; i < elements.length; i++) {elements[i].innerHTML = 'TEST';}"});
-
+  var elements = document.querySelectorAll('h2');
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].innerHTML = 'TEST';
+  }
 });
 
 
